9cdc27da39048101365c467d37f3231b5d821d82
Sean McPhee <seanmcphee@Seans-MacBook-Air.local>
2015-02-09 20:41:03 -0500

    Front end for therewolf TAGS tool

---
+++ js/default.js
@@ -0,0 +1,50 @@
+/* ==========================================================
+  Base Default JavaScript
+  -- Table of Contents --
+*/
+
+
+// JS functions and initiations go here...
+
+ $('.venue--categories__content').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  speed: 500,
+  asNavFor: '.venue--categories__nav'
+});
+$('.venue--categories__nav').slick({
+  slidesToShow: 3,
+  arrows: false,
+  slidesToScroll: 1,
+  asNavFor: '.venue--categories__content',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+  variableWidth: true,
+  infinite: false
+});
+//make sure the slider nav doesn't trigger page refresh
+$('.venue--categories__nav a').click(function(e){
+	e.preventDefault();
+});
+
+
+//Changes the add tags button state
+$('.onoffswitch input').change(function(){
+	if($(this).is(':checked')){
+		$(this).parents('.venue--category').find('.button--circle').removeClass('disabled');
+		$(this).parents('.venue--category').find('.tag').show(200);
+	}else{
+		$(this).parents('.venue--category').find('.button--circle').addClass('disabled');
+		$(this).parents('.venue--category').find('.tag').hide(200);
+	}
+});
+
+//Toggles the active class on like/dislike
+
+$('.tag-weight').click(function(e){
+	e.preventDefault();
+	$(this).siblings().removeClass('active');
+	$(this).toggleClass('active');
+});
